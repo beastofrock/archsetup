d582ce810206cd395f5c30c383012946e20d6886
beastofrock <45082804+beastofrock@users.noreply.github.com>
2020-11-30 21:25:19 +0100

    Update setup_stage01.sh

--- setup_stage01.sh
+++ setup_stage01.sh
@@ -1,6 +1,7 @@
 pacman -Syu
-echo 'mkfs.ext4 -L p_arch /dev/sdxY'
-echo 'mount -L p_arch /mnt'
+gdisk
+mkfs.ext4 -L p_arch /dev/sdxY
+mount -L p_arch /mnt
 pacstrap /mnt base linux linux-firmware dhcpcd nano git
 genfstab -Lp /mnt >> /mnt/etc/fstab
 arch-chroot /mnt/
