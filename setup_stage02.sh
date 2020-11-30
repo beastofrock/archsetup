83ed4480675ccc5b3cfc7688db3ca57a408272ea
beastofrock <45082804+beastofrock@users.noreply.github.com>
2020-11-30 20:31:41 +0100

    Create setup_stage02.sh

---
+++ setup_stage02.sh
@@ -0,0 +1,17 @@
+echo myhost > /etc/hostname
+echo LANG=de_DE.UTF-8 > /etc/locale.conf
+nano /etc/locale.gen
+locale-gen
+echo KEYMAP=de-latin1 > /etc/vconsole.conf
+echo FONT=lat9w-16 >> /etc/vconsole.conf
+echo KEYMAP=de-latin1 > /etc/vconsole.conf
+echo FONT=lat9w-16 >> /etc/vconsole.conf
+nano /etc/pacman.conf
+pacman -Sy
+mkinitcpio -p linux
+useradd rt
+mkdir -p /home/rt
+chown -R rt:rt /home/rt
+passwd rt
+echo 'now set root-password'
+passwd
