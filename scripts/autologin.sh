0d12b4b68699e48be8859e4606bb496b5d00181b
beastofrock <45082804+beastofrock@users.noreply.github.com>
2021-03-16 21:43:10 +0100

    Update autologin.sh

--- scripts/autologin.sh
+++ scripts/autologin.sh
@@ -1,6 +1,13 @@
 mkdir -pv /etc/systemd/system/getty@tty1.service.d/
-echo '  [Service]
+echo '[Service]
 ExecStart=
 ExecStart=-/sbin/agetty --autologin rt --noclear tty1 38400' > /etc/systemd/system/getty@tty1.service.d/autologin.conf
 systemctl enable getty@tty1.service
 systemctl status getty@tty1.service
+
+#mkdir -pv /etc/systemd/system/getty@tty2.service.d/
+#echo '[Service]
+#ExecStart=
+#ExecStart=-/sbin/agetty --autologin rt --noclear tty2 38400' > /etc/systemd/system/getty@tty2.service.d/autologin.conf
+#systemctl enable getty@tty2.service
+#systemctl status getty@tty2.service
