f4fff015aa85cef1b1434c7ffa017322486c44b0
beastofrock <45082804+beastofrock@users.noreply.github.com>
2021-03-18 00:24:50 +0100

    Update make_ap.sh

--- scripts/make_ap.sh
+++ scripts/make_ap.sh
@@ -1,23 +1,26 @@
-pacman -S iw hostapd wpa_supplicant wireless_tools iwd
-
+#pacman -S iw hostapd wpa_supplicant wireless_tools iwd
 ###rename interface
 ip link set wlp4s0 down
 ip link set wlp4s0 name wlan0
 ip link set wlan0 up
 ###/rename interface
-
 ip link set enp3s0 down
 ip link set enp3s0 name wlan0
 ip link set eth0 up
-
-
 ###test_wlan
+###set_ip_address_gateway_etc
+#ip link set eth0 up
+#ip add add dev eth0 192.168.0.x/24
+#ip route add default via 192.168.0.x
+#echo nameserver 192.168.0.x > /etc/resolv.conf
+###/set_ip_address_gateway_etc
+
+
 #iwctl
 #station wlan0 scan
 #station wlan0 get-networks
 #station wlan0 connect SSID
 ###/test_wlan
-
 ###make_bridge_br0
 
 
