e59f7fd06c4125fa3e6abe0d3abfe820e09cba7f
beastofrock <45082804+beastofrock@users.noreply.github.com>
2021-03-18 00:47:57 +0100

    Update make_ap.sh

--- scripts/make_ap.sh
+++ scripts/make_ap.sh
@@ -34,4 +34,68 @@ ping google.de -c 1
 ###/test_wlan
 ###make_bridge_br0
 
+/etc/hostapd/hostapd.conf:
 
+interface=wlan0
+#interface=br0
+logger_syslog=-1
+logger_syslog_level=2
+logger_stdout=-1
+logger_stdout_level=2
+ctrl_interface=/run/hostapd
+ctrl_interface_group=0
+ssid=omg!!!test
+hw_mode=g
+channel=3
+beacon_int=100
+dtim_period=2
+max_num_sta=255
+rts_threshold=-1
+fragm_threshold=-1
+macaddr_acl=1
+accept_mac_file=/etc/hostapd/accept
+auth_algs=3
+ignore_broadcast_ssid=0
+wmm_enabled=1
+wmm_ac_bk_cwmin=4
+wmm_ac_bk_cwmax=10
+wmm_ac_bk_aifs=7
+wmm_ac_bk_txop_limit=0
+wmm_ac_bk_acm=0
+wmm_ac_be_aifs=3
+wmm_ac_be_cwmin=4
+wmm_ac_be_cwmax=10
+wmm_ac_be_txop_limit=0
+wmm_ac_be_acm=0
+wmm_ac_vi_aifs=2
+wmm_ac_vi_cwmin=3
+wmm_ac_vi_cwmax=4
+wmm_ac_vi_txop_limit=94
+wmm_ac_vi_acm=0
+wmm_ac_vo_aifs=2
+wmm_ac_vo_cwmin=2
+wmm_ac_vo_cwmax=3
+wmm_ac_vo_txop_limit=47
+wmm_ac_vo_acm=0
+eapol_key_index_workaround=0
+eap_server=0
+#own_ip_addr=127.0.0.1
+own_ip_addr=192.168.0.9
+
+
+
+starting...
+#!/bin/bash
+sudo systemctl stop hostapd
+sudo iw dev wlan0 set 4addr on
+sudo brctl addif br0 wlan0
+sudo brctl show br0
+sudo systemctl start hostapd
+sudo hostapd_cli
+
+
+stoping...
+#!/bin/bash
+sudo systemctl stop hostapd
+sudo brctl delif br0 wlan0
+brctl show br0
