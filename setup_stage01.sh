8217651140a2347b23923b69f9dbd69c7f107ac6
beastofrock <45082804+beastofrock@users.noreply.github.com>
2021-07-26 13:44:43 +0200

    Update setup_stage01.sh

--- setup_stage01.sh
+++ setup_stage01.sh
@@ -1,6 +1,9 @@
 pacman -Syu
 #cfdisk
 #gdisk
+#gdisk > o new gpt
+#gdisk > n new partition > 512mb ef00 > OK
+#mkfs.fat -F 32 -n BOOT /dev/xY
 mkfs.ext4 -L p_arch /dev/sdxY
 mount -L p_arch /mnt
 pacstrap /mnt base linux linux-firmware dhcpcd nano git
