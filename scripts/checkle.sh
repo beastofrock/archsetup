0578227f55c3b26b4a743b3812737f2508f67c86
beastofrock <45082804+beastofrock@users.noreply.github.com>
2021-12-07 19:22:18 +0100

    Update checkle.sh

--- scripts/checkle.sh
+++ scripts/checkle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "enter url: "
 read url
-openssl s_client -servername $url -showcerts -connect $url:443
+openssl s_client -servername $url -showcerts -connect $url:443  | openssl x509 -noout -dates
