e7b74f7c65c31fc4b89e615aea47b491da5b7f4e
beastofrock <45082804+beastofrock@users.noreply.github.com>
2021-12-23 00:22:12 +0100

    Update sleaze-osd.sh

--- scripts/sleaze-osd.sh
+++ scripts/sleaze-osd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#pacman -S xosd
-while true; do curl https://onlineradiobox.com/de/sleazerock/playlist/ | grep uell -A2 | awk -v RS='<[^>]+>' -v ORS= '1' | osd_cat --align center --pos bottom --delay 8; sleep 40; done
+#pacman -Ss xosd
+while true; do curl https://onlineradiobox.com/de/sleazerock/playlist/ | grep uell -A1 | awk -v RS='<[^>]+>' -v ORS= '1' | osd_cat --align center --pos bottom --delay 8 --font -adobe-helvetica-bold-*-*-*-34-*-*-*-*-*-*-*; sleep 20; done
