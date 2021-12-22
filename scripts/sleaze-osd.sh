52f7844cc999d1d1f8ded2cfec23a42402de19f0
beastofrock <45082804+beastofrock@users.noreply.github.com>
2021-12-23 00:41:09 +0100

    Update sleaze-osd.sh

--- scripts/sleaze-osd.sh
+++ scripts/sleaze-osd.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 #pacman -Ss xosd
-while true; do curl https://onlineradiobox.com/de/sleazerock/playlist/ | grep uell -A1 | awk -v RS='<[^>]+>' -v ORS= '1' | osd_cat --align center --pos bottom --delay 8 --font -adobe-helvetica-bold-*-*-*-34-*-*-*-*-*-*-*; sleep 20; done
+#while true; do curl https://onlineradiobox.com/de/sleazerock/playlist/ | grep uell -A1 | awk -v RS='<[^>]+>' -v ORS= '1' | osd_cat --align center --pos bottom --delay 8 --font -adobe-helvetica-bold-*-*-*-34-*-*-*-*-*-*-*; sleep 20; done
+
+while true; do lynx -source https://onlineradiobox.com/de/sleazerock/playlist/ | \
+grep Live -A1 | \
+awk -v RS='<[^>]+>' -v ORS= '1' | \
+osd_cat --align center --pos bottom --delay 8 --font -adobe-helvetica-bold-*-*-*-34-*-*-*-*-*-*-* -c '#009BF9' -O 1; \
+sleep 20; \
+done
