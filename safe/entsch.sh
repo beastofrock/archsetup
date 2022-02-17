44ea5dadd043f4b5b596680e0b104d753a924640
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-02-18 00:56:12 +0100

    Create entsch.sh

---
+++ safe/entsch.sh
@@ -0,0 +1,8 @@
+echo "suche"
+read suche
+openssl enc -aes-256-cbc -md sha512 -pbkdf2 -salt -d -in file01.txt.enc -out file01.txt
+cat file01.txt | grep $suche
+echo "deleting...."
+rm file01.txt
+ls -lah
+echo "good.. no file here"
