3c1b0b07300d7649333ba4d46ec8d2bf039e3212
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-02-25 09:31:03 +0100

    Update setup_mssql.sh

--- scripts/setup_mssql.sh
+++ scripts/setup_mssql.sh
@@ -1,4 +1,4 @@
-apt install gnupg2 curl
+apt install gnupg2 curl htop wget 
 curl https://packages.microsoft.com/keys/microsoft.asc -o ms.asc
 cat ms.asc 
 apt-key add ms.asc 
@@ -6,5 +6,5 @@ apt-get install software-properties-common
 add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
 add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/debian/11/prod.list)"
 apt update
-apt install -y mssql-server
+apt install -y mssql-server mssql-tools
 /opt/mssql/bin/mssql-conf setup
