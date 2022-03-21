30ee200bc2f79fc623307a2060da1852ec5f1871
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-03-21 19:54:44 +0100

    Update geoip.sh

--- scripts/geoip.sh
+++ scripts/geoip.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-echo 'sudo journalctl -k | grep "IN=.*OUT=.*" for firwewall'
-
-echo "logfile? "
-read logfile
+#echo 'sudo journalctl -k | grep "IN=.*OUT=.*" for firwewall'
+#echo "logfile? "
+#read logfile
 # download geoip-database....
 # https://mailfud.org/geoip-legacy/GeoIP.dat.gz
 # gunzip GeoIP.dat.gz
 # filter ips
-cat $logfile | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"
+#cat $logfile | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"
 #sort
 #uniq
 #cat ./ip_test.txt | sort | uniq | xargs -n 1 geoiplookup { } 
+
+echo "
+sudo journalctl -k | grep "IN=.*OUT=.*" |  grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort | uniq | xargs -n 1 geoiplookup { } | sort | uniq -c | sort -n
+"
