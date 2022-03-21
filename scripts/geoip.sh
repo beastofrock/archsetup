f3993f885475b68b079f02d84098bf4c38dcaf71
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-03-21 19:41:28 +0100

    Update geoip.sh

--- scripts/geoip.sh
+++ scripts/geoip.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-echo "download geoip-database....
-
-wget https://mailfud.org/geoip-legacy/GeoIP.dat.gz
-gunzip GeoIP.dat.gz
-"
-
-echo "filter ips"
-
-echo 
-'cat data.txt | 
-grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"
-'
-
-sort
-uniq
-
-
-cat ./ip_test.txt | sort | uniq | xargs -n 1 geoiplookup { } 
+echo "logfile? "
+read logfile
+# download geoip-database....
+# https://mailfud.org/geoip-legacy/GeoIP.dat.gz
+# gunzip GeoIP.dat.gz
+# filter ips
+cat $logfile | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"
+#sort
+#uniq
+#cat ./ip_test.txt | sort | uniq | xargs -n 1 geoiplookup { } 
