de0ce828065c210c0072afaca45189217fc85261
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-03-22 00:12:35 +0100

    Create windyloop.sh

---
+++ scripts/windyloop.sh
@@ -0,0 +1,29 @@
+  GNU nano 6.2                                                                                      windyloop.sh                                                                                                  
+#!/bin/bash
+echo "starting Session..."
+echo "if ws > 7 (17,4 m/s) > raffstore goes up !!!!"
+
+#start "daemon in background"
+killall rtl_433
+rtl_433 -f 868300000 -X n=data,m=FSK_PCM,s=120,l=120,r=2500,match=Bresser-6 -Y classic -R 172 -F csv:op.csv > rtl.log 2>&1 &
+
+#declare function
+f_check () {
+a=$(tail -1 op.csv | cut -d ',' -f 16)
+b=2
+echo "$a"
+if (( $(echo "$a > $b" |bc -l) )); then
+   curl https://api.telegram.org/botYYYYYYYYYYYYYY:XXXXXXXXXXXXXXXXXXXX/sendMessage -d chat_id=88888888888 -d text="windig!!!! RAFFSTORE HOCH !!  $a m/s" &> /dev/null
+   echo "indeed"
+fi
+echo 'f_check_end'
+}
+
+#debug function:
+#f_check
+
+#starting loop
+while true
+do f_check
+sleep 5
+done
