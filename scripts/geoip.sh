f102b3b55d9f3565db3693968ae39b361b062992
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-03-21 19:58:08 +0100

    Update geoip.sh

--- scripts/geoip.sh
+++ scripts/geoip.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-#echo 'sudo journalctl -k | grep "IN=.*OUT=.*" for firwewall'
-#echo "logfile? "
-#read logfile
+cat geoip.sh
+
 # download geoip-database....
 # https://mailfud.org/geoip-legacy/GeoIP.dat.gz
 # gunzip GeoIP.dat.gz
 # filter ips
-#cat $logfile | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"
-#sort
-#uniq
-#cat ./ip_test.txt | sort | uniq | xargs -n 1 geoiplookup { } 
+# grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"
+# sort
+# uniq
+# xargs -n 1 geoiplookup { } 
+# sudo journalctl -k | grep "IN=.*OUT=.*" |  grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort | uniq | xargs -n 1 geoiplookup { } | sort | uniq -c | sort -n
 
-echo '
-sudo journalctl -k | grep "IN=.*OUT=.*" |  grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort | uniq | xargs -n 1 geoiplookup { } | sort | uniq -c | sort -n
-'
