0beb69765d93d4f7c34f7ef699d5e10d49ac18ac
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-04-04 22:27:32 +0200

    Update vulscan_nmap.sh

--- scripts/vulscan_nmap.sh
+++ scripts/vulscan_nmap.sh
@@ -7,5 +7,5 @@ rm -rf scipag_vulscan
 rm -rf nmap-vulners
 git clone https://github.com/vulnersCom/nmap-vulners.git
 git clone https://github.com/scipag/vulscan scipag_vulscan
-nmap -sV --script=~/vultemp/nmap-vulners/vulners.nse,~/vultemp/scipag_vulscan/vulscan.nse localhost
+nmap -sV --script=~/vultemp//nmap-vulners/vulners.nse,~/vultemp/scipag_vulscan/vulscan.nse localhost
 
