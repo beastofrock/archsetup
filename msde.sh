30d88b508baf0b9dd29f977a55c7b9effc61c86f
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-04-21 21:53:59 +0200

    Update msde.sh

--- msde.sh
+++ msde.sh
@@ -1,4 +1,4 @@
-pacman -S rofi xorg-server openbox xorg-fonts-misc xorg-xinit tint2 mplayer kate pcmanfm feh
+pacman -S rofi xorg-server openbox xorg-fonts-misc xorg-xinit tint2 mplayer kate pcmanfm feh breeze-gtk
 echo "setxkbmap de" >> .xinitrc
 echo "exec openbox-session" >> .xinitrc
 echo "$HOME/.bashrc" > .bash_profile
