021746d19b12e5dfa95cdadde5235d87ad6fa5de
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-04-21 23:55:19 +0200

    Update msde.sh

--- msde.sh
+++ msde.sh
@@ -1,6 +1,14 @@
-pacman -S rofi xorg-server openbox xorg-fonts-misc xorg-xinit tint2 mplayer kate pcmanfm feh breeze-gtk neofetch xcompmgr ranger 
+pacman -S rofi xorg-server openbox xorg-fonts-misc xorg-xinit tint2 mplayer kate pcmanfm feh breeze-gtk neofetch xcompmgr ranger pasystray doas
+
 echo "setxkbmap de" >> .xinitrc
 echo "exec openbox-session" >> .xinitrc
+
 echo "$HOME/.bashrc" > .bash_profile
 echo "startx" >> .bash_profile
 
+echo 'feh --bg-scale *.jpg' >> .config/openbox/autostart
+echo 'tint2 &' >> .config/openbox/autostart
+echo 'pasystray &' >> .config/openbox/autostart
+echo 'xcompmgr &' >> .config/openbox/autostart
+
+echo 'permit :wheel' > /etc/doas.conf
