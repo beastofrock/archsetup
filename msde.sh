be7b871565ada2aa2638275f3b4d8721b95a7082
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-05-04 17:34:53 +0000

    Update msde.sh

--- msde.sh
+++ msde.sh
@@ -1,11 +1,15 @@
+echo "
+
+copy .config to homefolder -R
+
+"
+
 echo "usermod -aG wheel $USER"
 
 pacman -S \
 rofi xorg-server openbox xorg-fonts-misc xorg-xinit tint2 mplayer \
 kate pcmanfm feh breeze-gtk neofetch xcompmgr ranger pasystray doas lxappearance \
 w3m highlight atool ueberzug neofetch pulseaudio dunst sudo git
-
-
 
 echo "setxkbmap de" >> .xinitrc
 echo "exec openbox-session" >> .xinitrc
