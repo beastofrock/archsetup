919fe081a011d3e69938fcefdb079f17c0a30449
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-07-07 20:57:11 +0200

    Update nat_router.sh

--- scripts/nat_router.sh
+++ scripts/nat_router.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+clear
+echo "0" > /proc/sys/net/ipv4/ip_forward
+iptables -X
+iptables -F
+iptables -t nat -F
+sleep 4
+ip a
+echo ""
+echo ""
 echo "enter wan_interface: "
 read wanif
 modprobe ip_tables
