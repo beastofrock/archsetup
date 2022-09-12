ac05248bd6691db0d7a74a554957da898ace8fd4
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-09-12 21:13:51 +0200

    Update entsch.sh

--- safe/entsch.sh
+++ safe/entsch.sh
@@ -1,7 +1,7 @@
 echo "suche"
 read suche
 openssl enc -aes-256-cbc -md sha512 -pbkdf2 -salt -d -in file01.txt.enc -out file01.txt
-cat file01.txt | grep $suche
+cat file01.txt | grep $suche | sed -e 's/;/,/g' | column -t -s,
 echo "deleting...."
 rm file01.txt
 ls -lah
