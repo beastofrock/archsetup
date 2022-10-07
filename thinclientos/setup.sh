3aff15dad36b37c3b2a5ee0323fccd81ec6e1751
beastofrock <45082804+beastofrock@users.noreply.github.com>
2022-10-07 11:23:11 +0200

    Update setup.sh

--- thinclientos/setup.sh
+++ thinclientos/setup.sh
@@ -2,7 +2,6 @@ apt update
 apt upgrade -y
 apt install xinit openbox rdesktop remmina git xterm wget feh -y
 
-
 mkdir -pv /etc/systemd/system/getty@tty1.service.d/
 echo '[Service]
 ExecStart=
@@ -11,14 +10,16 @@ systemctl enable getty@tty1.service
 systemctl status getty@tty1.service
 
 echo ' [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx ' >> ~/.bash_profile
-cat ~/.bash_profile
+cat /home/rt/.bash_profile
 
-mkdir ~/.config/openbox
-touch ~/.config/openbox/autostart
-chmod +x ~/.config/openbox/autostart
+mkdir /home/rt/.config/openbox
+touch /home/rt/.config/openbox/autostart
+chmod +x /home/rt/.config/openbox/autostart
+chown rt:rt -R /home/rt/.config/openbox
 echo '
 wget -N -O /home/rt/.local/share/remmina/group_rdp_terminalserver_ts01.remmina https://raw.githubusercontent.com/beastofrock/archsetup/main/thinclientos/ts01.txt
 remmina &
 feh --bg-max wallpaper.png' > ~/.config/openbox/autostart
-chmod +x ~/.config/openbox/autostart
+
+/sbin/reboot
 
